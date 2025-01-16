c207c1a770cec0a0f56c9a569ad54b6b01ca3e12
JinZhou5042 <jzhou24@nd.edu>
2025-01-16 10:32:13 -0500

    update

--- notebooks/skims/skimmer/ecf_skimmer.py
+++ notebooks/skims/skimmer/ecf_skimmer.py
@@ -15,65 +15,130 @@ import scipy
 
 full_start = time.time()
 
+
+def handle_wrapper_output(wrapper_output):
+    return
+
+def my_wrapper(key, fn, *args):
+    import random
+    import os
+    import string
+
+    characters = string.ascii_letters + string.digits
+    filename = ''.join(random.choices(characters, k=20))
+    temp_file_path = f"/scratch365/jzhou24/test/{filename}"
+
+    size_mb = random.randint(50, 200)
+    with open(temp_file_path, 'wb') as f:
+        f.write(os.urandom(size_mb * 1024 * 1024))
+
+    with open(temp_file_path, 'rb') as f:
+        data = f.read()
+
+    os.remove(temp_file_path)
+
+    dask_result = fn(*args)
+    wrapper_result = {"key": key, "dask_result": dask_result}
+    return wrapper_result, dask_result
+
+
 if __name__ == "__main__":
     m = DaskVine(
-        [8123, 8128],
-        name=f"{os.environ['USER']}-hgg",
-        run_info_path=f"/project01/ndcms/{os.environ['USER']}/vine-run-info",
+        [9123, 9128],
+        name=f"{os.environ['USER']}-hgg2",
     )
 
-    m.tune("temp-replica-count", 3)
+    #m.tune("temp-replica-count", 0)
     m.tune("transfer-temps-recovery", 1)
-    
+    # m.tune("wait-for-workers", 16)
+
     warnings.filterwarnings("ignore", "Found duplicate branch")
     warnings.filterwarnings("ignore", "Missing cross-reference index for")
     warnings.filterwarnings("ignore", "dcut")
     warnings.filterwarnings("ignore", "Please ensure")
     warnings.filterwarnings("ignore", "invalid value")
+
+    ######## Uncomment the following section if you need to create an input_datasets.json for new datasets ########
     
-    # with open('input_datasets.json', 'r') as f:
-    #     samples = json.load(f)
+    samples_path = '/project01/ndcms/cmoore24/samples'  ## Change this path to where data is
+    filelist = {}
+    categories = os.listdir(samples_path)
+    print(f"categories = {categories}")
 
-    # print('doing samples')
-    # sample_start = time.time()
+    for i in categories:
+        if '.root' in os.listdir(f'{samples_path}/{i}')[0]:
+            files = os.listdir(f'{samples_path}/{i}')
+            filelist[i] = [f'{samples_path}/{i}/{file}' for file in files]
+        else:
+            sub_cats = os.listdir(f'{samples_path}/{i}')
+            for j in sub_cats:
+                if '.root' in os.listdir(f'{samples_path}/{i}/{j}')[0]:
+                    files = os.listdir(f'{samples_path}/{i}/{j}')
+                    filelist[f'{i}_{j}'] = [f'{samples_path}/{i}/{j}/{file}' for file in files]
 
-    # @dask.delayed
-    # def sampler(samples):
-    #     samples_ready, samples = dataset_tools.preprocess(
-    #         samples,
-    #         step_size=50_000,
-    #         skip_bad_files=True,
-    #         recalculate_steps=True,
-    #         save_form=False,
-    #     )
-    #     return samples_ready
+    input_dict = {}
+    for i in filelist:
+        input_dict[i] = {}
+        input_dict[i]['files'] = {}
+        for j in filelist[i]:
+            input_dict[i]['files'][j] = {'object_path': 'Events'}
 
-    # sampler_dict = {}
-    # for i in samples:
-    #     sampler_dict[i] = sampler(samples[i])
+    print(f"dumping {len(input_dict)} samples, type = {type(input_dict)}")
+    with open('input_datasets.json', 'w') as fin:
+        json.dump(input_dict, fin)
+    
+    ######## End of previous section #########
+    
+    ######## Uncomment the following section if you need to pre-process the datasets present in input_datasets.json ########
+    
+    with open('input_datasets.json', 'r') as f:
+        samples = json.load(f)
 
-    # print('Compute')
-    # samples_postprocess = dask.compute(
-    #     sampler_dict,
-    #     scheduler=m.get,
-    #     resources={"cores": 1},
-    #     resources_mode=None,
-    #     prune_files=True,
-    #     lazy_transfers=True,
-    #     #task_mode="function_calls",
-    #     lib_resources={'cores': 12, 'slots': 12},
-    # )[0]
+    print('doing samples')
+    sample_start = time.time()
 
-    # samples_ready = {}
-    # for i in samples_postprocess:
-    #     samples_ready[i] = samples_postprocess[i]['files']
+    @dask.delayed
+    def sampler(samples):
+        samples_ready, samples = dataset_tools.preprocess(
+            samples,
+            step_size=50_000, ## Change this step size to adjust the size of chunks of events
+            skip_bad_files=True,
+            recalculate_steps=True,
+            save_form=False,
+        )
+        return samples_ready
 
-    # sample_stop = time.time()
-    # print('samples done')
-    # print('full sample time is ' + str((sample_stop - sample_start)/60))
+    sampler_dict = {}
+    for i in samples:
+        sampler_dict[i] = sampler(samples[i])
 
-    # with open("samples_ready.json", "w") as fout:
-    #     json.dump(samples_ready, fout)
+    print('Compute')
+    samples_postprocess = dask.compute(
+        sampler_dict,
+        scheduler=m.get,
+        resources={"cores": 1},
+        resources_mode=None,
+        prune_files=True,
+        lazy_transfers=True,
+        #task_mode="function_calls",
+        lib_resources={'cores': 12, 'slots': 12},
+    )[0]
+
+    samples_ready = {}
+    for i in samples_postprocess:
+        samples_ready[i] = samples_postprocess[i]['files']
+
+    sample_stop = time.time()
+    print('samples done')
+    print('full sample time is ' + str((sample_stop - sample_start)/60))
+
+    with open("samples_ready.json", "w") as fout:
+        json.dump(samples_ready, fout)
+
+    
+    ######## End of previous section ########
+
+    ######## The analysis portion begins here ########
 
     with open("samples_ready.json", 'r') as fin:
         samples_ready = json.load(fin)
@@ -92,7 +157,7 @@ if __name__ == "__main__":
         
         cut_to_fix_softdrop = (ak.num(events.FatJet.constituents.pf, axis=2) > 0)
         events = events[ak.all(cut_to_fix_softdrop, axis=1)]
-
+        
         trigger = ak.zeros_like(ak.firsts(events.FatJet.pt), dtype='bool')
         for t in triggers['2017']:
             if t in events.HLT.fields:
@@ -130,16 +195,17 @@ if __name__ == "__main__":
             axis=1,
         )
 
-        #nolepton = ((nmuons == 0) & (nelectrons == 0) & (ntaus == 0))
+        # nolepton = ((nmuons == 0) & (nelectrons == 0) & (ntaus == 0))
 
         onemuon = ((nmuons == 1) & (nelectrons == 0) & (ntaus == 0))
         # muonkin = ((leadingmuon.pt > 55.) & (abs(leadingmuon.eta) < 2.1))
         # muonDphiAK8 = (abs(leadingmuon.delta_phi(events.FatJet)) > 2*np.pi/3)
-
-
         
         # num_sub = ak.unflatten(num_subjets(events.FatJet, cluster_val=0.4), counts=ak.num(events.FatJet))
         # events['FatJet', 'num_subjets'] = num_sub
+
+        # region = nolepton ## Use this option to let more data through the cuts
+        region = onemuon ## Use this option to let less data through the cuts
 
 
         events['btag_count'] = ak.sum(events.Jet[(events.Jet.pt > 20) & (abs(events.Jet.eta) < 2.4)].btagDeepFlavB > 0.3040, axis=1)
@@ -160,7 +226,7 @@ if __name__ == "__main__":
                 & (abs(events.FatJet.eta) < 2.4)
                 & (events.FatJet.msoftdrop > 40)
                 & (events.FatJet.msoftdrop < 200)
-                & (onemuon)
+                & (region)
                 & (trigger)
             )
 
@@ -180,8 +246,8 @@ if __name__ == "__main__":
                 & (abs(events.FatJet.eta) < 2.4)
                 & (events.FatJet.msoftdrop > 40)
                 & (events.FatJet.msoftdrop < 200)
-                & (onemuon)
-                & (onemuon)
+                & (region)
+                & (trigger)
             )
 
         elif ('zqq' in dataset) or ('zz' in dataset):
@@ -200,7 +266,7 @@ if __name__ == "__main__":
                 & (abs(events.FatJet.eta) < 2.4)
                 & (events.FatJet.msoftdrop > 40)
                 & (events.FatJet.msoftdrop < 200)
-                & (onemuon)
+                & (region)
                 & (trigger)
             )
 
@@ -220,7 +286,7 @@ if __name__ == "__main__":
                 & (abs(events.FatJet.eta) < 2.4)
                 & (events.FatJet.msoftdrop > 40)
                 & (events.FatJet.msoftdrop < 200)
-                & (onemuon)
+                & (region)
                 & (trigger)
             )
     
@@ -232,8 +298,8 @@ if __name__ == "__main__":
                 & (abs(events.FatJet.eta) < 2.4)
                 & (events.FatJet.msoftdrop > 40)
                 & (events.FatJet.msoftdrop < 200)
+                & (region)
                 & (trigger)
-                & (onemuon)
             )
         
         events["goodjets"] = events.FatJet[fatjetSelect]
@@ -244,39 +310,6 @@ if __name__ == "__main__":
         events['goodjets', 'color_ring'] = ak.unflatten(
              color_ring(events.goodjets, cluster_val=0.4), counts=ak.num(events.goodjets)
         )
-        
-        # events['goodjets', 'd2b1'] = ak.unflatten(
-        #      d2_calc(events.goodjets), counts=ak.num(events.goodjets)
-        # )
-        
-        # events['goodjets', 'u1'] = ak.unflatten(
-        #      u_calc(events.goodjets, 1), counts=ak.num(events.goodjets)
-        # )
-        
-        # events['goodjets', 'u2'] = ak.unflatten(
-        #      u_calc(events.goodjets, 2), counts=ak.num(events.goodjets)
-        # )
-        
-        # events['goodjets', 'u3'] = ak.unflatten(
-        #      u_calc(events.goodjets, 3), counts=ak.num(events.goodjets)
-        # )
-        
-        # events['goodjets', 'd3'] = ak.unflatten(
-        #      d3_calc(events.goodjets), counts=ak.num(events.goodjets)
-        # )
-        
-        # events['goodjets', 'm2'] = ak.unflatten(
-        #      m2_calc(events.goodjets), counts=ak.num(events.goodjets)
-        # )
-        
-        # events['goodjets', 'm3'] = ak.unflatten(
-        #      m3_calc(events.goodjets), counts=ak.num(events.goodjets)
-        # )
-        
-        # events['goodjets', 'n4'] = ak.unflatten(
-        #      n4_calc(events.goodjets), counts=ak.num(events.goodjets)
-        # )
-
 
         jetdef = fastjet.JetDefinition(
             fastjet.cambridge_algorithm, 0.8
@@ -286,7 +319,8 @@ if __name__ == "__main__":
         softdrop = cluster.exclusive_jets_softdrop_grooming()
         softdrop_cluster = fastjet.ClusterSequence(softdrop.constituents, jetdef)
         
-        for n in range(2,6):
+        # (2, 3) -> (2, 6)
+        for n in range(2, 6):
             for v in range(1, int(scipy.special.binom(n, 2))+1):
                 for b in range(5, 45, 5):
                     ecf_name = f'{v}e{n}^{b/10}'
@@ -298,7 +332,7 @@ if __name__ == "__main__":
         events["ecfs"] = ak.zip(ecfs)
 
         if (('hgg' in dataset) or ('hbb' in dataset) or ('wqq' in dataset) or ('ww' in dataset) or ('zqq' in dataset) or ('zz' in dataset) or ('wz' in dataset)):
-
+            return
             skim = ak.zip(
                 {
                     "Color_Ring": events.goodjets.color_ring,
@@ -314,6 +348,7 @@ if __name__ == "__main__":
                 depth_limit=1,
             )
         else:
+            
             skim = ak.zip(
                 {
                     "Color_Ring": events.goodjets.color_ring,
@@ -332,52 +367,62 @@ if __name__ == "__main__":
         skim_task = dak.to_parquet(
             #events,
             skim,
-            f"/project01/ndcms/cmoore24/skims/ecfs/test/{dataset}/",
+            f"/scratch365/jzhou24/ecf_calculator_output/{dataset}/", ##Change this to where you'd like the output to be written
             compute=False,
         )
         return skim_task
 
+    ###### Uncomment this regeion if you want to run only one of the subsamples found in input_datasets.json ######
 
-    # subset = {}
-    # to_skim = 'ttboosted_700to1000'
-    # subset[to_skim] = samples_ready[to_skim]
-    # files = subset[to_skim]['files']
-    # form = subset[to_skim]['form']
-    # dict_meta = subset[to_skim]['metadata']
-    # keys = list(files.keys())
+    subset = {}
 
-    # batch = {}
-    # batch[to_skim] = {}
-    # batch[to_skim]['files'] = {}
-    # batch[to_skim]['form'] = form
-    # batch[to_skim]['metadata'] = dict_meta
+    for key, value in samples_ready.items():
+        print(key, len(value['files']))
 
-    # for i in range(0, len(files)):
-    # for i in range(len(files)):
-    #     batch[to_skim]['files'][keys[i]] = files[keys[i]]
+    to_skim = 'SingleMuon_Run2017B_240303_183047' ## Use this string to choose the subsample. Name must be found in input_datasets.json ######
+    subset[to_skim] = samples_ready[to_skim]
+    files = subset[to_skim]['files']
+    form = subset[to_skim]['form']
+    dict_meta = subset[to_skim]['metadata']
+    keys = list(files.keys())
+
+    batch = {}
+    batch[to_skim] = {}
+    batch[to_skim]['files'] = {}
+    batch[to_skim]['form'] = form
+    batch[to_skim]['metadata'] = dict_meta
+
+    for i in range(0, len(files)):
+        for i in range(len(files)):
+            batch[to_skim]['files'][keys[i]] = files[keys[i]]
+
     
     tasks = dataset_tools.apply_to_fileset(
         analysis,
-        #dataset_tools.slice_files(samples_ready, slice(None, 20)),
-        #dataset_tools.slice_files(batch, slice(None, 100, None)),
-        samples_ready,
-        #batch,
+        # samples_ready, ## Run over all subsamples in input_datasets.json
+        batch, ## Run over only the subsample specified as the "to_skim" string
         uproot_options={"allow_read_errors_with_report": False},
         schemaclass = PFNanoAODSchema,
     )#[0]
 
-
-    print('start compute')
+    print(f"start compute")
+    
     computed = dask.compute(
             tasks,
             scheduler=m.get,
             resources={"cores": 1},
             resources_mode=None,
-            lazy_transfers=True,
             prune_files=True,
+            worker_transfers=True,   # False: bring all files back to the manager
+            # env_vars={'PATH': '/scratch365/jzhou24/env/bin/:$PATH'},    # shared filesystem
+            #scheduling_mode='small-input-first',
+            #wrapper=my_wrapper,
+            #wrapper_proc=handle_wrapper_output,
             #task_mode="function_calls",
-            lib_resources={'cores': 12, 'slots': 12},
+            #lib_resources={'cores': 12, 'slots': 12},
         )
 
     full_stop = time.time()
     print('full run time is ' + str((full_stop - full_start)/60))
+
+    #print(f"shutdown {m.workers_shutdown()} workers")
